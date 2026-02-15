f71875fe63d87c921c96c4b682923aca8f9047d9
Adam Koniak <akoniak@gmail.com>
2026-02-15 13:14:45 -0800

    Initial Commit

---
+++ Cognac Cabin App IOS/Cognac_Cabin_App_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cognac_Cabin_App_IOSApp.swift
+//  Cognac Cabin App IOS
+//
+//  Created by Adam Koniak on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Cognac_Cabin_App_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cognac Cabin App IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cognac Cabin App IOS
+//
+//  Created by Adam Koniak on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
